5070cd116f34b21c7536b2195d0920c864c72a75
KS Durazo <sdurazo.UNLV@gmail.com>
2022-03-29 20:54:12 -0700

    added pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
 // TODO: Declare any global variables we need
+// number of heads: 0
+// number of tails: 0
+// imageElement: node
+// resultMessageElement: node
+
+// function updateTable content
+    // set heads text content to numberOfHeads
+    // set tails text content to numberOfTails
+    // if (numberOfHeads + numberOfTails)
+        // set percentageHeads to (numberOfHeads/(numberOfHeads + numberOfTails))
+        // set percentageTails to (numberOfTails/(numberOfHeads + numberOfTails))
+        // else
+            // set percentageHeads to 0
+            // set percentageTails to 
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -7,21 +21,28 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Hi')
 
     // TODO: Add event listener and handler for flip and clear buttons
+    // select flip button and add event listener
+        // listening for a click
+        // on click
+            // create isHeads variable
+            // see if it's heads or tails
+                // generate a random number
+                    // if odd isHeads = true
+                    // if even isHeads = false
+            // if isHeads
+                // add one to heads
+                // change text content to "You flipped heads!"
+                // change src property on imageElement to './assets/images/pennny-heads.jpg'
+                // update table content
+             // if !isHeads
+                // add one to tails
+                // change text content to "You flipped tails!"
+                // change src property on imageElement to './assets/images/pennny-tails.jpg'
+                // update table content
 
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
-
-})
+    // select clear button and add event listener
+        // listening for a click
+            // numberOfHeads to 0
+            // numberOfTails to 0
+            // Update Table Content
+            // change text content to 'Let's get to flipping!'
